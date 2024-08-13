9c43dd42cdb4b8d36e6697107fd11e6b1962007b
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-08-13 22:49:54 +0530

    File updated
    File updated.

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+export const prompts = [
+    "A futuristic cityscape at sunset",
+    "A mystical forest with glowing plants",
+    "A serene beach with crystal clear water",
+    "A bustling street market in an exotic location",
+    "A tranquil mountain lake with reflections",
+    "A vibrant underwater coral reef",
+    "A majestic castle on a hilltop",
+];
