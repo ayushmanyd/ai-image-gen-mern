745c775b96090ef95943fa5a99dbd5f560c14273
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-08-25 23:36:50 +0530

    App.jsx file updated
    App.jsx file updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home, CreateNewPost } from "./Pages";
+import logo from "./assets/logo.svg";
 
+const App = () => {
+  return ();
+};
 
 export default App;
