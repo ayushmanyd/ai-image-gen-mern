7c22563f7a8299f6ac12b8680e2edf1bb637b190
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-08-29 23:59:16 +0530

    Home.jsx file updated
    Home.jsx file updated.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import { Card, Loader, FormField } from "../components";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
     <>
     </>
