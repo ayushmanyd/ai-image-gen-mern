fd345f436abe249d2471fe3ccc4893dbc19e2847
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-09-02 23:54:21 +0530

    App.jsx updated
    App.jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,11 @@ const App = () => {
         >
           Create
         </Link>
-        <Link
-          to="/create-post"
-          className="font-poppins font-medium bg-[#00A2E8] text-white px-4 py-2 rounded-md"
-        >
-          Create
-        </Link>
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        
+        
+      </main>
     </BrowserRouter>
   );
 };
