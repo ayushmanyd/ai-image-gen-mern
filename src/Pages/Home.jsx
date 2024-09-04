a999150b83fea390b3ccd74476af3277e0a18b0a
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-09-04 23:11:31 +0530

    Home.jsx updated
    Home.jsx updated.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,6 +12,10 @@ const Home = () => {
         <h1 className="font-extrabold text-[#222328] text-[32px]">
           Explore others Post
         </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Browse through a collection of already generated stunning images
+        </p>
+
       </div>
     </section>
   );
