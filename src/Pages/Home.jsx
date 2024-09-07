0017e286ed3847dd38bf12acc835b90bbcf3fc34
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-09-07 21:43:46 +0530

    Home.jsx updated
    Home.jsx updated.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,6 +44,16 @@ const Home = () => {
                   <span className="text-[#222328]">{searchText}</span>
                 </h2>
               )}
+              <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+                {searchText ? (
+                  <RenderCards
+                    data={[]}
+                    title="No search results found"
+                  />
+                ) : (
+                  <RenderCards data={[]} title="No posts found" />
+                )}
+              </div>
             </>
           )}
         </div>
