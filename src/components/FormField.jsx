47ab585fe2536b31ffd973de58557dcefc8b4729
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-09-10 22:24:39 +0530

    FormField.jsx added
    FormField.jsx added.

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({
+  LabelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+    </div>
+  );
 };
 
 export default FormField;
