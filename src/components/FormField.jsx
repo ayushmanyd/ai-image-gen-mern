51a233bfca6c6952415adc9e8e214b24b38552cf
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-09-11 22:20:24 +0530

    FormField.jsx updated
    FormField.jsx updated.

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -19,6 +19,15 @@ const FormField = ({
         >
           {LabelName}
         </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
       </div>
     </div>
   );
