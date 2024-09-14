b10448d967ed83dc3f95c3326cf0bf45ab8d299a
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-09-14 23:55:47 +0530

    CreateNewPost.jsx updated
    CreateNewPost.jsx updated.

--- src/Pages/CreateNewPost.jsx
+++ src/Pages/CreateNewPost.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreateNewPost = () => {
-  return <div>CreateNewPost</div>;
 };
 
 export default CreateNewPost;
