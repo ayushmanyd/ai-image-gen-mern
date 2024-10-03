bd093c2eefa334f48b13e575d446cf0201bbfe23
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-10-03 21:12:57 +0530

    Index.js error fixed
    Index.js error fixed.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
-app.use("/api/v1/dRoutes", dRoutes);
+app.use("/api/v1/d", dRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello World");
