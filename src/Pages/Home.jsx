7ed802331d528e5ff4e2f36414fbc3dcf0352729
ayushmanyd <ayushmanpyad@gmail.com>
2024-10-19 23:58:54 +0530

    Home.jsx updated
    Home.jsx updated.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -67,6 +67,15 @@ const Home = () => {
 
   return (
     <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Explore others Post
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Browse through a collection of already generated stunning images
+        </p>
+      </div>
+
       <div className="mt-10">
         {loading ? (
           <div className="flex justify-center items-center">
