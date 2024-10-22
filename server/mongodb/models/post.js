9c6c5fd2c9a92081daf924c49c698471e55d57f7
ayushmanyd <155382910+ayushmanyd@users.noreply.github.com>
2024-10-22 23:17:05 +0530

    post.js updated
    post.js updated.

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -18,6 +18,11 @@ router.route("/").get(async (req, res) => {
   try {
     const posts = await Post.find({});
     res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
   }
 });
 
