edd75bf1bfe80fcc6ff6f83b0a0f2f538c2d8386
ayushmanyd <155382910+ayushmanyd@users.noreply.github.com>
2024-10-23 21:53:13 +0530

    post.js updated
    post.js updated.

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -36,6 +36,14 @@ router.route("/").post(async (req, res) => {
       prompt,
       photo: photoUrl.url,
     });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
 });
 
 export default router;
